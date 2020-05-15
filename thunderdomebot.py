b550e0b751cd5049b0a257a08210e8bf88b479c1
Galen Savidge <galensavidge@gmail.com>
2020-05-15 15:06:31 -0700

    Reading history and counting reactions works!

--- thunderdomebot.py
+++ thunderdomebot.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="tdb!")
 
+database = {}
+
 @bot.event
 async def on_ready():
     print("Discord bot online!")
@@ -14,11 +16,11 @@ async def on_ready():
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    write_to_sql(reaction.message.author, reaction.emoji, 1)
+    write_to_db(reaction.message.author, reaction.emoji, 1)
 
 @bot.event
 async def on_reaction_remove(reaction, user):
-    write_to_sql(reaction.message.author, reaction.emoji, -1)
+    write_to_db(reaction.message.author, reaction.emoji, -1)
 
 @bot.event
 async def on_guild_join(guild):
@@ -28,8 +30,8 @@ async def on_guild_join(guild):
 async def get_reactions(ctx, emoji: str, user=None):
     users = ctx.message.mentions
     for user in users:
-        count = read_from_sql(user, emoji)
-        ctx.send("User {0} has received {1} {2}", user.name, "no" if count == 0 else str(count), str(emoji))
+        count = read_from_db(user, emoji)
+        await ctx.send("User {0} has received {1} {2}".format(user.name, "no" if count == 0 else str(count), str(emoji)))
 
 
 async def read_message_history(guild):
@@ -42,16 +44,37 @@ async def read_message_history(guild):
                     user = message.author
                     emoji = reaction.emoji
                     count = reaction.count
-                    print("user: "+user.name+" emoji: "+str(emoji)+" count: "+str(count))
-                    write_to_sql(user, emoji, count)
+                    write_to_db(user, str(emoji), count)
         print("parsed "+str(messages_parsed)+" messages in "+channel.name)
 
-def write_to_sql(user, emoji, count):
-    pass
+def write_to_db(user, emoji: str, count):
+    if user in database:
+        sub_db = database[user]
+        if emoji in sub_db:
+            new_count = sub_db.get(emoji) + count
+        else:
+            new_count = count
+        
+        if new_count > 0:
+            sub_db[emoji] = new_count
+        else:
+            del sub_db[emoji]
+    else:
+        if count > 0:
+            database[user] = {emoji : count}
+        
 
-def read_from_sql(user, emoji):
-    count = 0
-    return count
+def read_from_db(user, emoji: str):
+    if user in database:
+        sub_db = database[user]
+        if emoji in sub_db:
+            return sub_db[emoji]
+        else:
+            print("no record of "+emoji+" for the user!")
+    else:
+        print("user not in database!")
+    
+    return 0
 
 
 if __name__ == "__main__":
