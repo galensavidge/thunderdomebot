8cbaeeb89d1d2e43acffe0c78fcd1130f5dff8a8
Galen Savidge <galensavidge@gmail.com>
2020-05-20 18:29:40 -0700

    Fixed datatypes, hopefully

--- create_table.py
+++ create_table.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     db = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = db.cursor(cursor_factory=DictCursor)
 
-    cursor.execute("CREATE TABLE messages (message_id BIGINT PRIMARY KEY, author_id BIGINT, emoji TINYTEXT, count INT)")
+    cursor.execute("CREATE TABLE messages (message_id BIGINT PRIMARY KEY, author_id BIGINT, emoji VARCHAR(128), count INT)")
     db.commit()
